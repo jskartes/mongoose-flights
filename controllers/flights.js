7b4e2a5b1d1eb4de8d81387ec37c0a7aa67ca8c3
jskartes <joshuakartes@gmail.com>
2023-12-02 10:58:52 -0700

    implement create flight functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,25 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-  index 
+  index,
+  new: newFlight,
+  create
 };
 
-function index(req, res) {
-  res.render('flights/index');
+async function index(req, res) {
+  const flights = await Flight.find({});
+  res.render('flights/index', { flights });
+}
+
+function newFlight(req, res) {
+  res.render('flights/new');
+}
+
+async function create(req, res) {
+  try {
+    await Flight.create(req.body);
+    res.redirect('/flights');
+  } catch (err) {
+    console.log(err);
+  }
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const flightsController = require('../controllers/flights');
 
 router.get('/', flightsController.index);
+router.get('/new', flightsController.new);
+router.post('/', flightsController.create);
 
 module.exports = router;
