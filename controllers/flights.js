3901aa4aa7976d09ad45f1abf09aa91825f0b6e5
jskartes <joshuakartes@gmail.com>
2023-12-02 15:21:50 -0700

    incorporate flight destination functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,6 +2,7 @@ const Flight = require('../models/flight');
 
 module.exports = {
   index,
+  show,
   new: newFlight,
   create
 };
@@ -9,6 +10,11 @@ module.exports = {
 async function index(req, res) {
   const flights = await Flight.find({}).sort({ departs: 'asc' });
   res.render('flights/index', { flights });
+}
+
+async function show(req, res) {
+  const flight = await Flight.findById(req.params.id);
+  res.render('flights/show', { flight });
 }
 
 function newFlight(req, res) {
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,7 @@ const flightsController = require('../controllers/flights');
 
 router.get('/', flightsController.index);
 router.get('/new', flightsController.new);
+router.get('/:id', flightsController.show);
 router.post('/', flightsController.create);
 
 module.exports = router;
